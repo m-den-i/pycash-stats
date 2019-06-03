96e65790ef25f03508151e87e621dfa7feb6cdb3
Denis Makedonov <denis.makedonov@gmail.com>
2019-06-03 22:38:51 +0300

    Fix app.py

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def hello_world():
-    s = open_book(os.path.join(base_dir, 'CommonDB.gnucash'))
-    print(s.default_currency)
+    db_path = os.environ['DB_PATH']
     rows = []
-    for tr in sorted(s.transactions, key=lambda x: x.post_date):
-        rows.append({
-            'name': "- {:%Y/%m/%d} : {}".format(tr.post_date, tr.description),
-            'splits': [
-                "\t{amount}  {direction}  {account} : {memo}".format(
-                    amount=abs(spl.value),
-                    direction="-->" if spl.value > 0 else "paid by",
-                    account=spl.account.fullname,
-                    memo=spl.memo
-                ) for spl in tr.splits
-            ]
-        })
+    with open_book(db_path) as s:
+        for tr in sorted(s.transactions, key=lambda x: x.post_date):
+            rows.append({
+                'name': "- {:%Y/%m/%d} : {}".format(tr.post_date, tr.description),
+                'splits': [
+                    "\t{amount}  {direction}  {account} : {memo}".format(
+                        amount=abs(spl.value),
+                        direction="-->" if spl.value > 0 else "paid by",
+                        account=spl.account.fullname,
+                        memo=spl.memo
+                    ) for spl in tr.splits
+                ]
+            })
     return render_template('index.html', **{'rows': rows})
 
 
